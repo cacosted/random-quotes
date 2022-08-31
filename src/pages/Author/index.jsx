9572c6e1ead0f82858c5499bc6c3934ef0a3af5d
Clemente Acosta <clems.acosta@gmail.com>
2022-08-30 20:47:11 -0500

    replace return word with back arrow icon

--- src/pages/Author/index.jsx
+++ src/pages/Author/index.jsx
@@ -1,9 +1,10 @@
+import { useEffect } from 'react'
 import { useQuote } from '../../hooks/useQuote'
 import { QuoteContainer } from '../../components/QuoteContainer'
 import { Quote } from '../../components/Quote'
 import { NavBar } from '../../components/NavBar'
 import { Link } from 'wouter'
-import { useEffect } from 'react'
+import { FaArrowLeft } from 'react-icons/fa'
 export const Author = ({ params }) => {
   const name = params.name.replaceAll('%20', ' ')
   const { quoteList, setAuthorQuotes } = useQuote()
@@ -14,8 +15,8 @@ export const Author = ({ params }) => {
   return (
     <div>
       <NavBar>
+        <Link to='/'><FaArrowLeft /></Link>
         <h1>{name}</h1>
-        <Link to='/'>Return</Link>
       </NavBar>
       {quoteList.map(item => {
         return (
