2f8027103b1fc3c1162e279d807f2dc4f99ca427
Clemente Acosta <clems.acosta@gmail.com>
2022-08-30 22:17:48 -0500

    fix name format issue on Author page

--- src/pages/Author/index.jsx
+++ src/pages/Author/index.jsx
@@ -5,14 +5,13 @@ import { Link } from 'wouter'
 import { FaArrowLeft } from 'react-icons/fa'
 import { useQuoteList } from '../../hooks/useQuoteList'
 export const Author = ({ params }) => {
-  const name = params.name.replaceAll('%20', ' ')
-  const [quoteList] = useQuoteList(name)
-
+  const [quoteList] = useQuoteList(params.name)
+  const name = quoteList[0]?.author
   return (
     <div>
       <NavBar>
         <Link to='/'><a><FaArrowLeft /></a></Link>
-        <a>{name}</a>
+        <h1>{name}</h1>
       </NavBar>
       {quoteList.map(item => {
         return (
